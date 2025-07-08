439de026803b4a89c702498435e7dfb573eb69db
DESKTOP-27SPJ0Q\Admin <alapsoni@live.com>
2025-07-08 20:31:24 +1000

    Bike controls and grid added

--- src/Grid.java
+++ src/Grid.java
@@ -1,3 +1,13 @@
 public class Grid {
-    
+     private final int width;
+    private final int height;
+
+    public Grid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public boolean isValidPosition(int x, int y) {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
 }
