eb498a33f7ef95c751a412d4bc7f5f2d31d1920f
DESKTOP-27SPJ0Q\Admin <alapsoni@live.com>
2025-07-08 20:09:42 +1000

    Bike methods

--- src/Bike.java
+++ src/Bike.java
@@ -1,3 +1,39 @@
 public class Bike {
-    
+    private int x;
+    private int y;
+    private Direction facing;
+    private boolean isPlaced;
+
+    public Bike() {
+        this.isPlaced = false;
+    }
+
+ public boolean place(int x, int y, Direction facing, Grid grid) 
+ {
+return true;
+        }
+
+public void forward(Grid grid) {
+        if (!isPlaced) {
+            return;
+        }
+
+    }
+
+public void turnLeft() {
+       
+    }
+
+   public void turnRight() {
+       
+    }
+
+    public String gpsReport() {
+       
+    return "";
+    }
+public boolean isPlaced() {
+        return isPlaced;
+    }
+
 }
